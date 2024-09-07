178d84746d44e6fd3f9b583250f2ebfac8edd76c
Andrei <andreiflausino@live.com>
2024-09-06 23:26:32 -0300

    AboutMe

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,11 +6,25 @@ function AboutMe() {
         Hello! 👋🏻 I&apos;m Andrei Flausino, a Full-Stack Web Developer from Brazil who enjoys creating fun and interactive
         webapps. <br />
         <br />
-        I&apos;ve always been passionate about computers since I was young. Constantly fiddling around with computer files,
-        modding video games, and installing all sorts of stuff on my parent&apos;s computer. <br />
-        {/* After a lot of thinking, I finally decided that the obvious carrer path for me had to be Software Engineering. <br />I
-        attended college at <a href="https://www.anhanguera.com/">Anhanguera</a>, getting a degree in Information Technology. */}
-        Eventually I got a degree in Information Technology and have been improving my programming skills ever since.
+        I&apos;ve always been passionate about computers since I was young. I vivdly remember constantly messing around with my
+        dad&apos;s Windows XP. When I got a bit older I got into modding video games, configuring them to play the way I wanted
+        them to.
+        <br />
+        Being this obscessed with computers made me get a degree in Information Technology. And since then, I have been improving
+        my programming skills, playing around with projects, and learning new technologies whenever possible.
+      </p>
+
+      <p>
+        Olá! 👋🏻 Eu sou Andrei Flausino, Brasileiro e Desenvolvedor Web Full-Stack! Eu adoro criar websites divertidos e
+        interativos.
+        <br />
+        <br />
+        Eu sempre gostei de computadores, lembro que quando era pequeno, eu adorava fuçar nos arquivos que meu pai tinha guardado
+        no Windows XP. Quando um pouco mais velho, comecei a gostar de modificar os jogos que eu baixava, configurando-os de
+        acordo com a minha preferência. <br />
+        Sendo obcecado por computadores me fez eventualmente cursar e obter o diploma de tecnólogo em Análise e Desenvolvimento de
+        Sistemas. E desde então, eu continuo a melhorar como programador, estudando, criando projetos, e aprendendo novas
+        tecnologias sempre que possível.
       </p>
     </>
   );
